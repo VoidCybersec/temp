e757b352c81ef10fcfd67662291c5eb82a789598
Thigendra Colin Dupuis <95173140+VoidCybersec@users.noreply.github.com>
2023-01-05 00:39:59 +0800

    added a file setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update
+apt install git
+apt install python3.10-venv
+apt install python3-pip
+apt ins
+git clone https://github.com/BlehPoppy/TP051894-FYP
+cd TP051894-FYP
+python3 -m venv ~/virtualenv/home_nmap/
+. ~/virtualenv/home_nmap/bin/activate
+pip install -r requirements.txt
+python setup.py bdist_wheel
+pip install dist/home_nmap-0.0.1-py3-none-any.whl
+
+echo "All done Sunshine ;)"
